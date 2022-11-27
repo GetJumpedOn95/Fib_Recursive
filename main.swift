8d460ae1fecf06c52434de344f749e44ebc9e4f9
Brayden Edington <brayden.edington@student.allenisd.org>
2022-11-27 11:45:31 -0600

    Initial commit

---
+++ main.swift
@@ -0,0 +1,26 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func fibonacci(n:Int) -> Int {
+    var result = 0
+    print("Calculating fibonacci(\(n))...")
+    if n <= 2 {
+        print("Terminal case, returning 1.")
+        return 1
+    }else{
+        result = fibonacci(n:n-1) + fibonacci(n:n-2)
+        print("Non-terminal case, returning \(result).")
+        return result
+    }
+}
+
+var fibResult = fibonacci(n:n)
+print("The final result is: \(fibResult).")
